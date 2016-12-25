8b4433a2c694bc15eefca26e146dabb620e7b780
Yovan Juggoo <yovan.juggoo.php@gmail.com>
2016-12-25 07:06:04 +0400

    More notes added

--- Sites/ecommerce/src/MainBundle/Controller/DefaultController.php
+++ Sites/ecommerce/src/MainBundle/Controller/DefaultController.php
@@ -6,7 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-    public function indexAction($name=null)
+    public function indexAction($name = null)
     {
         return $this->render('MainBundle:Default:index.html.twig', ['name' => $name]);
     }
